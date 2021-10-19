cbc635379652f9b7b7417bd6c44135d9c81de86c
Natanael <NatanaelUF17@gmail.com>
2021-10-19 09:42:46 -0400

    Finishing up the todo app 👨🏽‍💻

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,23 +8,54 @@ function App() {
     setNewTodo(event.target.value);
   }, []);
 
+  // TODO: Adding the todo values to the todos arrays and clean up the input
   const formSubmitted = useCallback(
     (event) => {
       event.preventDefault();
       if (!newTodo.trim()) return;
       setTodos([
-        ...todos,
         {
-          id: todos.length + 1,
+          id: todos.length ? todos[0].id + 1 : 1,
           content: newTodo,
           done: false,
         },
+        ...todos
       ]);
       setNewTodo("");
     },
     [newTodo, todos]
   );
 
+  // TODO: Adding new todo to the array
+  const addTodo = useCallback(
+    (todo, index) => (event) => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1, {
+        ...todo,
+        done: !todo.done,
+      });
+      setTodos(newTodos);
+    },
+    [todos]
+  );
+  
+  // TODO: Remove specific todo from the todos array
+  const removeTodo = useCallback((todo) => (event) => {
+    setTodos(todos.filter(otherTodo => otherTodo !== todo));
+  }, [todos]);
+
+  // TODO: Mark all todo done
+  const markAllDone = useCallback(() => {
+    const updatedTodos = todos.map((todo) => {
+      return {
+        ...todo,
+        done: true
+      }
+    });
+    setTodos(updatedTodos);
+  }, [todos]);
+
+  // TODO: Using useEffect to call todos once at the beginning
   useEffect(() => {
     console.log("todos", todos);
   }, [todos]);
@@ -39,15 +70,19 @@ function App() {
           value={newTodo}
           onChange={onNewTodoChange}
         />
-        <button>Add Todo</button>
+        <button onClick={addTodo}>Add Todo</button>
       </form>
+      <button onClick={markAllDone}>Mark All Done</button>
       <ul>
-        {todos.map((todo) => (
+        {todos.map((todo, index) => (
           <li key={todo.id}>
-            <input 
-              type="checkbox" 
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={addTodo(todo, index)}
             />
-            {todo.content}
+            <span className={todo.done ? 'done' : ''}>{todo.content}</span>
+            <button onClick={removeTodo(todo)}>Remove Todo</button>
           </li>
         ))}
       </ul>
